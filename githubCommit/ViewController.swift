a64e8f38e73a2a3a0bbe18b5783d2a1ec530a5cd
Hemanth Kumar <ev_mac17@ev-mac17s-mac.local>
2017-12-16 11:33:50 +0530

    Final Commit

--- githubCommit/ViewController.swift
+++ githubCommit/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "GitHub"
+        self.title = "GitHubFinal"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
